2f5988a79befc930a15622b9a9c50b478e8c32f0
onlysavior <onlysavior@foxmail.com>
2013-12-07 20:25:38 +0800

    test my github issue

--- src/test/java/onlysavior/mybatis/common/EntityTest.java
+++ src/test/java/onlysavior/mybatis/common/EntityTest.java
@@ -1,11 +1,5 @@
-package onlysavior.mybatis.common;
+﻿package onlysavior.mybatis.common;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Administrator
- * Date: 13-12-6
- * Time: 下午11:37
- * To change this template use File | Settings | File Templates.
- */
+
 public class EntityTest {
 }
